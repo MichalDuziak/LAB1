29e498c717d4f42e1e0f34c7dfeff7244d236e52
Michal <Michal@MICHA>
2024-05-06 23:53:02 +0200

    Projekt

---
+++ LAB1/Projekt/Projek1t.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ZarzadzanieZadaniami
+{
+    [DataContract]
+    public class Zadanie
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nazwa { get; set; }
+        [DataMember]
+        public string Opis { get; set; }
+        [DataMember]
+        public DateTime DataZakonczenia { get; set; }
+        [DataMember]
+        public bool CzyWykonane { get; set; }
+
+        public Zadanie(int id, string nazwa, string opis, DateTime dataZakonczenia, bool czyWykonane)
+        {
+            Id = id;
+            Nazwa = nazwa;
+            Opis = opis;
+            DataZakonczenia = dataZakonczenia;
+            CzyWykonane = czyWykonane;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Nazwa: {Nazwa}, Opis: {Opis}, Data zakończenia: {DataZakonczenia}, Wykonane: {(CzyWykonane ? "Tak" : "Nie")}";
+        }
+    }
+}
---
+++ LAB1/Projekt/Projekt2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+
+namespace ZarzadzanieZadaniami
+{
+    public class ManagerZadan
+    {
+        private List<Zadanie> listaZadan;
+
+        public ManagerZadan()
+        {
+            listaZadan = new List<Zadanie>();
+        }
+
+        public void DodajZadanie(Zadanie zadanie)
+        {
+            listaZadan.Add(zadanie);
+        }
+
+        public void UsunZadanie(int id)
+        {
+            listaZadan.RemoveAll(z => z.Id == id);
+        }
+
+        public void WyswietlZadania()
+        {
+            foreach (var zadanie in listaZadan)
+            {
+                Console.WriteLine(zadanie);
+            }
+        }
+
+        public void ZapiszDoPlikuXML(string sciezka)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Zadanie>));
+            using (TextWriter writer = new StreamWriter(sciezka))
+            {
+                serializer.Serialize(writer, listaZadan);
+            }
+        }
+
+        public void WczytajZPlikuXML(string sciezka)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Zadanie>));
+            using (TextReader reader = new StreamReader(sciezka))
+            {
+                listaZadan = (List<Zadanie>)serializer.Deserialize(reader);
+            }
+        }
+
+        public void ZapiszDoPlikuJSON(string sciezka)
+        {
+            using (var fs = new FileStream(sciezka, FileMode.Create))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Zadanie>));
+                serializer.WriteObject(fs, listaZadan);
+            }
+        }
+
+        public void WczytajZPlikuJSON(string sciezka)
+        {
+            using (var fs = new FileStream(sciezka, FileMode.Open))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Zadanie>));
+                listaZadan = (List<Zadanie>)serializer.ReadObject(fs);
+            }
+        }
+    }
+}
