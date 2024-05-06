69b4e0fd94225427baf51da2d35e06db7f93d849
Michal <Michal@MICHA>
2024-05-06 23:21:46 +0200

    Zad 6

---
+++ LAB1/Zadanie 6/copyFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class copyFile
+{
+    static void Zad6()
+    {
+        Console.Write("Podaj ścieżkę do pliku źródłowego: ");
+        string sciezkaZrodlo = Console.ReadLine();
+
+        Console.Write("Podaj ścieżkę do pliku docelowego: ");
+        string sciezkaDocelowa = Console.ReadLine();
+
+        if (File.Exists(sciezkaZrodlo))
+        {
+            KopiujPlik(sciezkaZrodlo, sciezkaDocelowa);
+            Console.WriteLine("Plik został pomyślnie skopiowany.");
+        }
+        else
+        {
+            Console.WriteLine("Plik źródłowy nie istnieje.");
+        }
+
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć program");
+        Console.ReadKey();
+    }
+
+    static void KopiujPlik(string sciezkaZrodlo, string sciezkaDocelowy)
+    {
+        using (FileStream zrodloFs = new FileStream(sciezkaZrodlo, FileMode.Open))
+        {
+            using (FileStream docelowyFs = new FileStream(sciezkaDocelowy, FileMode.Create))
+            {
+                zrodloFs.CopyTo(docelowyFs);
+            }
+        }
+    }
+}
