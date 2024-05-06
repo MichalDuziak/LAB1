c39299f012abb0323c235b4e0e005cc94f6610c4
Michal <Michal@MICHA>
2024-05-06 23:10:23 +0200

    zad4

---
+++ LAB1/Zadanie 4/Zad4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Zad4
+{
+    static void revert()
+    {
+        string sciezkaDoPliku = "C:\\Users\\Michal\\source\\repos\\LAB1\\LAB1\\Zadanie 3\\plik.txt";
+
+        using (StreamReader sr = new StreamReader(sciezkaDoPliku))
+        {
+            string linia;
+            while ((linia = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(OdwrocLinie(linia));
+            }
+        }
+
+        Console.ReadKey();
+    }
+
+    static string OdwrocLinie(string linia)
+    {
+        char[] odwrocona = new char[linia.Length];
+        for (int i = 0; i < linia.Length; i++)
+        {
+            odwrocona[i] = linia[linia.Length - 1 - i];
+        }
+        return new string(odwrocona);
+    }
+}
